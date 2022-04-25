87531719b84504db74da2165259cc64220de72aa
Alexander Cirac Antio <95984829+AlexanderCirac@users.noreply.github.com>
2022-04-25 12:12:04 +0200

    Only refactoring 2.2.9.1
    Just refactor 2.2.9.1 with my new knowledge

--- Assets/Scripts/InGame/Player/SC_PlayerController.cs
+++ Assets/Scripts/InGame/Player/SC_PlayerController.cs
@@ -1,20 +1,19 @@
 using UnityEngine;
-using System.Collections;
 using UnityEngine.UI;
 using C_Thorn.UI;
 using System;
+
 namespace C_Thorn.InGame.Player
 {
     public class SC_PlayerController : MonoBehaviour
     {
-
           #region Attributes
           [Header("Button to move player")]
-          [SerializeField] private Button _playerButton;
-          [SerializeField] private Camera _playerCamera;
+          [SerializeField] Button _playerButton;
+          [SerializeField] Camera _playerCamera;
 
           //to the animation ship or player
-          private Vector3 _initialVec3;
+          Vector3 _initialVec3;
           [Header("SpaceShip to animate")]
           [SerializeField] private GameObject _shipObject;
           
@@ -24,94 +23,52 @@ namespace C_Thorn.InGame.Player
           #endregion
 
           #region UnityCall
-          private void Awake()
-          {
-              _initialVec3 = this.transform.position;
-          }
-          void Start()
-          {   
-              _playerButton.GetComponent<SC_UISpecialButtonToHandler>().OnHold.AddListener(ToMove);
-          }
+          void Awake() => _initialVec3 = this.transform.position;
+          void Start() => _playerButton.GetComponent<SC_UISpecialButtonToHandler>().OnHold.AddListener(ToMove);
 
-          private void Update()
-          {
-              ToAnimationShip();
-          }
+          void Update() => ToAnimationShip();
 
-          private void OnTriggerEnter(Collider _col)
+          void OnTriggerEnter(Collider _col)
           {
               if (_col.CompareTag("PowerUPPoints"))
               {
-                  if(OnReloadPoints != null)
-                  {
-                      OnReloadPoints();
-                  }
+                  OnReloadPoints?.Invoke();
                   Destroy(_col.gameObject);
               }              
-      
               if (_col.CompareTag("Killer"))
-              {
                   Destroy(this.gameObject);
-              }
-              
               if (_col.CompareTag("PowerUPTime"))
               {
-                  if(OnIncresTime != null)
-                  {
-                      OnIncresTime();
-                  }
+                  OnIncresTime?.Invoke();
                   Destroy(_col.gameObject);
               }
-
           }
           #endregion
 
-          #region Methods
-          private void ToMove()
+          #region Custom private Methods
+          void ToMove()
           {
               Touch touch = Input.GetTouch(0);
               Vector3 mousePosition = new Vector3(touch.position.x, touch.position.y, 65);
               Vector3 objPosition = _playerCamera.ScreenToWorldPoint(mousePosition);
 
-              if (Input.mousePosition.x < (Screen.width - (Screen.width / 10)) && (Input.mousePosition.x > ((Screen.width - System.Math.Abs(Input.mousePosition.x)) - ((Screen.width) - (Screen.width / 6)))))
-              {
-                  transform.position = new Vector3(objPosition.x, transform.position.y, transform.position.z);
-              }
-              else
-              {
-                  transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
-              }
-
-
-              if (Input.mousePosition.y < (Screen.height - (Screen.height / 5)) && (Input.mousePosition.y > ((Screen.height - System.Math.Abs(Input.mousePosition.y)) - ((Screen.height) - (Screen.height / 3) - 50))))
-              {
-                  transform.position = new Vector3(transform.position.x, objPosition.y, objPosition.z);
-              }
-              else
-              {
-                  transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
-              }
+              transform.position = _isInScreenX ? new Vector3(objPosition.x, transform.position.y, transform.position.z) : new Vector3(transform.position.x, transform.position.y, transform.position.z);
+              
+              transform.position = _isInScreenY ? new Vector3(transform.position.x, objPosition.y, objPosition.z) : new Vector3(transform.position.x, transform.position.y, transform.position.z);
+            
               _initialVec3 = this.transform.position;
           }
-          private void ToAnimationShip()
+          void ToAnimationShip()
           {
-              if (this.transform.position.x > _initialVec3.x)
-              {
-                  if (_shipObject.transform.rotation.z > -0.3)
-                  {
-                      _shipObject.transform.RotateAround(_shipObject.gameObject.transform.position, Vector3.forward, -500 * Time.deltaTime);
-                  }
-              }
-              if (_shipObject.transform.position.x < _initialVec3.x)
-              {
-                  if (_shipObject.transform.rotation.z < 0.3)
-                  {
-                      _shipObject.transform.RotateAround(_shipObject.gameObject.transform.position, Vector3.forward, 500 * Time.deltaTime);
-                  }
-              }
-          }
-          #endregion
+              if (this.transform.position.x > _initialVec3.x && _shipObject.transform.rotation.z > -0.3)
+                  _shipObject.transform.RotateAround(_shipObject.gameObject.transform.position, Vector3.forward, -500 * Time.deltaTime);
 
+              if (_shipObject.transform.position.x < _initialVec3.x && _shipObject.transform.rotation.z < 0.3)
+                 _shipObject.transform.RotateAround(_shipObject.gameObject.transform.position, Vector3.forward, 500 * Time.deltaTime);
+          }
+          bool _isInScreenX { get => (Input.mousePosition.x < (Screen.width - (Screen.width / 10)) && (Input.mousePosition.x > ((Screen.width - System.Math.Abs(Input.mousePosition.x)) - ((Screen.width) - (Screen.width / 6))))); }          
+          bool _isInScreenY { get => (Input.mousePosition.y < (Screen.height - (Screen.height / 5)) && (Input.mousePosition.y > ((Screen.height - System.Math.Abs(Input.mousePosition.y)) - ((Screen.height) - (Screen.height / 3) - 50)))); }
+          #endregion
     }
 
 }
